10472a77d06efb5abe1fea180a8dd4147039ecee
Allen Olivares <aolivares@catalyte.io>
2026-07-13 14:47:00 -0500

    Made changes to PurchaseRepository

--- repositories/purchase_repository.py
+++ repositories/purchase_repository.py
@@ -3,20 +3,25 @@ from models.purchase import Purchase
 class PurchaseRepository:
     def __init__(self):
         self._purchases=list[Purchase] = []
-    def get_all(self):
+    def get_all(self) ->list[Purchase]:
         return self._purchases
-    def get_by_id(self,id:int):
+    def get_by_id(self,id:int) -> Purchase | None:
         return ((p for p in self._purchases if p.id == id), None)
     def add(self, purchase: Purchase) -> Purchase:
         self._purchases.append(purchase)
         return purchase
-    def update(self, id: int, purchase: Purchase):
+    def update(self, id: int, purchase: Purchase) ->Purchase|None:
         for ind,p in enumerate(self._purchases):
             if p.id ==id:
                 self._purchases[ind] = purchase
-        return None
-    def delete(self, id: int):
+                return purchase
+            else: 
+                return None
+    def delete(self, id: int)->bool:
         for p in self._purchases:
             if p.id == id:
                 self._purchases.remove(p)
+                return True
+            else:
+                return False
 
