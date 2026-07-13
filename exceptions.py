c85c0445ef7a87b2a26d06f1f7178f8b203d1ecb
Allen Olivares <aolivares@catalyte.io>
2026-07-13 15:37:21 -0500

    Created exceptions and changed purchase_service

---
+++ exceptions.py
@@ -0,0 +1,6 @@
+class Incorrect_date_format(Exception):
+    """Raised when the date formate does not match YYYY-MM-DD HH:MM:SS TZ"""
+    pass
+class DuplicatePurchaseError(Exception):
+    """Raised when there has been a purchase with a certain id already created"""
+    pass
--- services/purchase_service.py
+++ services/purchase_service.py
@@ -1,6 +1,7 @@
 from repositories.purchase_repository import PurchaseRepository
 from models.purchase import Purchase
-from exceptions import DuplicatePurchaseError
+from exceptions import DuplicatePurchaseError,Incorrect_date_format
+from datetime import datetime
 
 class PurchaseService:
     def __init__(self, repository: PurchaseRepository):
@@ -9,5 +10,10 @@ class PurchaseService:
     def create_purchase(self, purchase: Purchase) -> Purchase:
         if self._repository.get_by_id(purchase.id) is not None:
             raise DuplicatePurchaseError(f"Purchase '{purchase.id}' already exists.")
+        correct_format = "%Y-%m-%d %H:%M:%S UTC"
+        date_string=self._repository.get_by_id(purchase.id).timestamp
+        try:
+            datetime.strptime(date_string, correct_format)
+        except ValueError:
+            raise Incorrect_date_format(f"Timestamp {date_string} is not in the right format")
         return self._repository.add(purchase)
-    
