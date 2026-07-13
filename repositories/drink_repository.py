e6a7ef9f0bf0121cfbcaef7a04adb06989575f02
Bryan Verble <bverble@catalyte.io>
2026-07-13 14:13:29 -0500

    Add DrinkRepository

---
+++ repositories/drink_repository.py
@@ -0,0 +1,37 @@
+from models.drink import Drink
+
+class DrinkRepository:
+    def __init__(self):
+        self.drinks: list[Drink] = []
+
+    def get_all(self):
+        return self.drinks
+
+    def get_by_id(self, id: int) -> Drink | None:
+        return next((d for d in self._drinks if d.id == id), None)
+
+    def add(self, drink: Drink) -> Drink:
+        self.drinks.append(drink)
+        return drink
+
+    def update(self, id: int, drink:Drink) -> Drink | None:
+        ix = None
+        for drink, i in enumerate(self.drinks):
+            if id == drink.id:
+                ix = i
+        if ix:
+            self.drinks[ix] = drink
+            return drink
+        else:
+            return None
+
+    def delete(self, id: int) -> bool:
+        ix = None
+        for drink, i in enumerate(self.drinks):
+            if id == drink.id:
+                ix = i
+        if ix:
+            self.drinks.pop(ix)
+            return True
+        else:
+            return False
