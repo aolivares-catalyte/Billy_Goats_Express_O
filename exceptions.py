9f4eaaea58af4a80ea6ae48d6ca842e54bb93e3b
Worst Generation <cwilson@catalyte.io>
2026-07-14 10:46:03 -0500

    added doc str for exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,11 +1,13 @@
 
-class Incorrect_date_format(Exception):
+class IncorrectDateFormat(Exception):
     """Raised when the date formate does not match YYYY-MM-DD HH:MM:SS TZ"""
     pass
 class DuplicatePurchaseError(Exception):
     """Raised when there has been a purchase with a certain id already created"""
     pass
 class DuplicateBakedGoodError(Exception):
+    """Raised when there is a duplicate entry for a baked good"""
     pass
 class BakedGoodNotFoundError(Exception):
+    """raised when there is no result for the baked good search"""
     pass
