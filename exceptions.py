bf06460ddc501994f9b6f36e49738df7401769a6
Worst Generation <cwilson@catalyte.io>
2026-07-14 10:51:04 -0500

    added missing Exception

--- exceptions.py
+++ exceptions.py
@@ -1,9 +1,10 @@
-
 class IncorrectDateFormat(Exception):
     """Raised when the date formate does not match YYYY-MM-DD HH:MM:SS TZ"""
     pass
 class DuplicatePurchaseError(Exception):
     """Raised when there has been a purchase with a certain id already created"""
+    pass
+class DuplicateDrinkError(Exception):
     pass
 class DuplicateBakedGoodError(Exception):
     """Raised when there is a duplicate entry for a baked good"""
