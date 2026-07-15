58360146e153c8b19553c6a17e440f0d92547db7
Bryan Verble <bverble@catalyte.io>
2026-07-15 11:38:26 -0500

    Add documentation to CustomerRepository

--- repositories/customer_repository.py
+++ repositories/customer_repository.py
@@ -1,23 +1,60 @@
 from models.customer import Customer
 
 class CustomerRepository:
+    """Manages the list of Customers."""
+
     def __init__(self):
         self.customers: list[Customer] = []
 
-    def get_all(self):
+    def get_all(self) -> list[Customer]:
+        """Return a list containing all Customers in the repository."""
         return self.customers
 
     def get_by_id(self, id: int) -> Customer | None:
+        """Search the repository for a Customer with the provided id.
+
+        Args:
+            id: The Customer ID to search for.
+
+        Returns:
+            The Customer with the specified ID, or None if not found.
+        """
         return next((c for c in self.customers if c.id == id), None)
 
     def get_by_name(self, name: str) -> Customer | None:
+        """Search the repository for a Customer with the provided name.
+
+        Args:
+            name: The Customer name to search for.
+
+        Returns:
+            The Customer with the specified name, or None if not found.
+        """
         return next((c for c in self.customers if c.name == name), None)
 
     def add(self, customer: Customer) -> Customer:
+        """Add a Customer to the repository.
+
+        Args:
+            customer: The Customer to add.
+
+        Returns:
+            The Customer that was added.
+        """
         self.customers.append(customer)
         return customer
 
     def update(self, id: int, customer: Customer) -> Customer | None:
+        """Replace the Customer with the specified ID.
+
+        Args:
+            id: The ID of the Customer to be removed.
+            customer: The Customer to add in its place.
+
+        Returns:
+            The Customer that was added, or None if no Customer with the provided ID
+            was found.
+        """
         customer_index = next((i for (i, c) in enumerate(self.customers) if c.id == id), None)
         if customer_index is not None:
             self.customers[customer_index] = customer
@@ -26,6 +63,14 @@ class CustomerRepository:
             return None
 
     def delete(self, id: int) -> bool:
+        """Remove the Customer with the specified ID.
+
+        Args:
+            id: The ID of the Customer to be removed.
+
+        Returns:
+            True if the Customer was found and removed, False if not found.
+        """
         customer_index = next((i for (i, c) in enumerate(self.customers) if c.id == id), None)
         if customer_index is not None:
             self.customers.pop(customer_index)
