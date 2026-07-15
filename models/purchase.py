367ec4823b0889e8239f23580d60666e1d041ff9
Allen Olivares <aolivares@catalyte.io>
2026-07-15 16:00:16 -0500

    Fixes

--- models/purchase.py
+++ models/purchase.py
@@ -8,15 +8,14 @@ class Purchase:
     id:int
     items:list
     customer:Customer
-
     timestamp:datetime = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
-    
     total_cost:Decimal = field(init=False)
     
     
     def __post_init__(self):
-        self.items = []
         self.total_cost = sum(i.sale_price for i in self.items)
+
+
 
 
         
