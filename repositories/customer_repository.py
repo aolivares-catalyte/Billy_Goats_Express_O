6216c2460c546f16bdce87920ff2b791d0549d82
Bryan Verble <bverble@catalyte.io>
2026-07-15 11:40:06 -0500

    Rename customers attribute of CustomerRepository

--- repositories/customer_repository.py
+++ repositories/customer_repository.py
@@ -4,11 +4,11 @@ class CustomerRepository:
     """Manages the list of Customers."""
 
     def __init__(self):
-        self.customers: list[Customer] = []
+        self._customers: list[Customer] = []
 
     def get_all(self) -> list[Customer]:
         """Return a list containing all Customers in the repository."""
-        return self.customers
+        return self._customers
 
     def get_by_id(self, id: int) -> Customer | None:
         """Search the repository for a Customer with the provided id.
@@ -19,7 +19,7 @@ class CustomerRepository:
         Returns:
             The Customer with the specified ID, or None if not found.
         """
-        return next((c for c in self.customers if c.id == id), None)
+        return next((c for c in self._customers if c.id == id), None)
 
     def get_by_name(self, name: str) -> Customer | None:
         """Search the repository for a Customer with the provided name.
@@ -30,7 +30,7 @@ class CustomerRepository:
         Returns:
             The Customer with the specified name, or None if not found.
         """
-        return next((c for c in self.customers if c.name == name), None)
+        return next((c for c in self._customers if c.name == name), None)
 
     def add(self, customer: Customer) -> Customer:
         """Add a Customer to the repository.
@@ -41,7 +41,7 @@ class CustomerRepository:
         Returns:
             The Customer that was added.
         """
-        self.customers.append(customer)
+        self._customers.append(customer)
         return customer
 
     def update(self, id: int, customer: Customer) -> Customer | None:
@@ -55,9 +55,9 @@ class CustomerRepository:
             The Customer that was added, or None if no Customer with the provided ID
             was found.
         """
-        customer_index = next((i for (i, c) in enumerate(self.customers) if c.id == id), None)
+        customer_index = next((i for (i, c) in enumerate(self._customers) if c.id == id), None)
         if customer_index is not None:
-            self.customers[customer_index] = customer
+            self._customers[customer_index] = customer
             return customer
         else:
             return None
@@ -71,9 +71,9 @@ class CustomerRepository:
         Returns:
             True if the Customer was found and removed, False if not found.
         """
-        customer_index = next((i for (i, c) in enumerate(self.customers) if c.id == id), None)
+        customer_index = next((i for (i, c) in enumerate(self._customers) if c.id == id), None)
         if customer_index is not None:
-            self.customers.pop(customer_index)
+            self._customers.pop(customer_index)
             return True
         else:
             return False
